fa3cda8286d6455812f92cb43253db60a0f972ce
Illustre13 <ndahayosibertin17@gmail.com>
2022-03-10 21:15:20 +0200

    0x00hell_world

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//int randomnumber;
+	//int lastdegit;
+	
+	//randomnumber = 25;
+	//lastdegit = randomnumber % 10;
+	
+	//printf("the last degit is: %d", lastdegit);
+
+	char a;
+
+	
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	
+	return (0);
+}

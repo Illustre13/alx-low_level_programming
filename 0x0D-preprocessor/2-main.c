5132964261d1910fcf9486890164ed98eb88b114
Illustre13 <ndahayosibertin17@gmail.com>
2022-04-11 20:35:35 +0000

    Task 2: Function that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from,
+ * with a new line
+ *
+ * return : 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
